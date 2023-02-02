97c2a483210f4a31f29171c42829e3ea8cec618d
Roland Hajós <hajosr7@gmail.com>
2023-02-02 12:40:00 +0100

    backend equipment

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const EmployeeModel = require("./db/employee.model");
+const EquipmentModel = require("./db/equipment.model")
 
 const { MONGO_URL, PORT = 8080 } = process.env;
 
@@ -38,6 +39,64 @@ app.get("/api/employees/", async (req, res) => {
 
 app.get("/api/employees/:id", (req, res) => {
   return res.json(req.employee);
+});
+
+//routes for equipments
+app.use("/api/equipments/:id", async (req, res, next) => {
+  let equipment = null;
+
+  try {
+    equipment = await EquipmentModel.findById(req.params.id);
+  } catch (err) {
+    return next(err);
+  }
+
+  if (!equipment) {
+    return res.status(404).end("Equipment not found");
+  }
+
+  req.equipment = equipment;
+  next();
+});
+
+app.get("/api/equipment/", async (req, res) => {
+  const equipment = await EquipmentModel.find().sort({ created: "desc" });
+  return res.json(equipment);
+});
+
+app.get("/api/equipment/:id", (req, res) => {
+  return res.json(req.equipment);
+});
+
+app.post("/api/equipment/", async (req, res, next) => {
+  const equipment = req.body;
+
+  try {
+    const saved = await EquipmentModel.create(equipment);
+    return res.json(saved);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+app.patch("/api/equipment/:id", async (req, res, next) => {
+  const equipment = req.body;
+
+  try {
+    const updated = await req.equipment.set(equipment).save();
+    return res.json(updated);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+app.delete("/api/equipment/:id", async (req, res, next) => {
+  try {
+    const deleted = await req.equipment.delete();
+    return res.json(deleted);
+  } catch (err) {
+    return next(err);
+  }
 });
 
 //route for Robert
